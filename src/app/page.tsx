2268330a7558fc138d3ea952de00c244a3ecac3b
Mayankkamriya <kamriyamayank45@gmail.com>
2025-01-17 20:12:12 +0530

    Banner is ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Banner from "../Components/Banner"
 
 export default function Home() {
-  return (
-  <>
+  return (<>
+
+  <Banner/>
     <div>In Home page </div>
+  {/* // </Banner> */}
+
   </>
   );
 }
