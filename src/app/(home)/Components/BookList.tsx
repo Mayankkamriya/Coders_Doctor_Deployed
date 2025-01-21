1c7ba1e83f0c4e9d4b11de5cdeeb55815d596b45
Mayankkamriya <kamriyamayank45@gmail.com>
2025-01-21 18:58:01 +0530

    API optimization and adding loading....

--- src/app/(home)/Components/BookList.tsx
+++ src/app/(home)/Components/BookList.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import { Book } from '@/src/types'
 import BookCart from './BookCart'
 
-const BookList = ({ books }: { books: Book[] }) => {
+const BookList = async() => {
+  const response = await fetch(`${process.env.BOOKLIST_URL_DEP}/list`);
+
+  if(!response.ok){
+    throw new Error('An error occured while fetching the Doctors')
+  }
+  const encodedData = await response.json();
+  
+    const books = JSON.parse(atob(encodedData.data));
+    console.log('Decoded Books Data...:', books);
+
   return (
     <div className='grid grid-cols-1 gap-8 md:grid-cols-3 max-w-7xl mx-auto mb-10'>
       {
-        books.map((book) => (
+        books.map((book:Book) => (
           // <h1 key={book._id}>{book._id},{book.name}</h1>
           <BookCart key={book._id} book={book}/> 
         ))
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,17 @@
+import { Suspense } from "react";
 import Banner from "./Components/Banner"
 import BookList from "./Components/BookList";
+import Loading from "@/src/Components/Loading";
 
 export default async function Home() {
 
-  const response = await fetch(`${process.env.BOOKLIST_URL_DEP}/list`);
-
-  if(!response.ok){
-    throw new Error('An error occured while fetching the Doctors')
-  }
-  const encodedData = await response.json();
-  
-    const books = JSON.parse(atob(encodedData.data));
-    // console.log('Decoded Books Data...:', books);
 
   return (<>
 
   <Banner/>
-  <BookList books={books}/>
+  <Suspense fallback={<Loading/>}>
+    <BookList />
+  </Suspense>
 
   </>
   );
